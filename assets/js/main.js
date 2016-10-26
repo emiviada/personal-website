75f75149ecf2a1e2c8b733f07a1f32c27e83292c
Emiliano Viada <emjovi@gmail.com>
2016-10-26 00:16:35 -0300

    Fixed Google map bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -460,23 +460,6 @@ $(document).ready(function($){
 
 }(jQuery));
 
-// Google Maps functionality
-var map;
-function initMap() {
-    const cba = {lat: -31.416667, lng: -64.183333};
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: cba,
-      zoom: 4
-    });
-    // Add marker
-    var marker = new google.maps.Marker({
-        position: cba,
-        map: map,
-        icon: 'assets/images/marker-icon.png',
-		title: 'Emiliano Viada\'s location'
-    });
-}
-
 $(window).load(function(){
 
 	// section calling
